6d9336cd768173bc9bd6c14adb9de25685e8490b
Mads Opheim <mads.opheim@gmail.com>
2018-12-30 22:06:45 +0100

    Renamed init to main

--- scrapy_facebooker/main.py
+++ scrapy_facebooker/main.py
@@ -9,10 +9,12 @@ def getProcess():
         'USER_AGENT': 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
     })
  
-def run():
+def fetch():
     process = getProcess()
     for page in pages:
         process.crawl(FacebookEventSpider, page=page)
     process.start()
 
-run()
+
+def run(request):
+    fetch()
